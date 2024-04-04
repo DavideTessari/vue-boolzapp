c6cfff5d407deccae66a5f6c7ec0ea84d1afcaa8
DavideTessari <davtex@gmail.com>
2024-04-04 12:02:11 +0200

    milstone 3 completed

--- js/script.js
+++ js/script.js
@@ -4,89 +4,109 @@ createApp({
     data() {
         return {
             contacts: [{
-                name: 'Michele',
-                avatar: '_1',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Fabio',
-                avatar: '_2',
-                visible: true,
-                messages: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                  },
-                  {
-                    date: '20/03/2020 16:35:00',
-                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Samuele',
-                avatar: '_3',
-                visible: true,
-                messages: [{
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                  },
-                  {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Luisa',
-                avatar: '_4',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                  }
-                ],
-              },
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [{
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [{
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [{
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [{
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
             ],
+            currentContactIndex: 0,
+            newMessageText: ''
         };
     },
     methods: {
+        addMessage() {
+            const currentDate = new Date().toLocaleString();
+            const newMessage = {
+                date: currentDate,
+                message: this.newMessageText,
+                status: 'sent'
+            };
+            this.contacts[this.currentContactIndex].messages.push(newMessage);
+            // Dopo 1 secondo, invia la risposta "ok" dall'interlocutore
+            setTimeout(() => {
+                const okMessage = {
+                    date: currentDate,
+                    message: 'ok',
+                    status: 'received'
+                };
+                this.contacts[this.currentContactIndex].messages.push(okMessage);
+            }, 1000);
+            // Resetta l'input del messaggio
+            this.newMessageText = '';
+        }
     }
 }).mount('#app');
-
-
